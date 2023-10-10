c3fd58f2cc0a8d16f56ec03680c0163af6dae701
angelguzman95 <147192360+angelguzman95@users.noreply.github.com>
2023-10-09 20:32:55 -0600

    Add files via upload

---
+++ promedio practica 1.cpp
@@ -0,0 +1,38 @@
+//incluir la libreria
+#include<iostream>
+/* utilizar todos los caracteres y funciones 
+de la libreria
+*/
+using namespace std;
+// funcion principal
+int main(){
+	// declarar variable 
+	int a,b,c,d,e,suma,promedio;
+	cout<<"bienvenido a este programa que dira tu promedio";
+	cout<<"dime el primer promedio"<<endl;
+	cin>>a;
+	cout<<"dime el segundo promedio"<<endl;
+	cin>>b;
+	cout<<"dime el tercer promedio"<<endl;
+	cin>>c;
+	cout<<"dime el cuarto promedio"<<endl;
+	cin>>d;
+	cout<<"dime el quinto promedio"<<endl;
+	cin>>e;
+	promedio = (a+b+c+d+e)/5;
+	cout<<"tu promedio general es de:"<<promedio<<endl;
+	if(promedio >=7){
+		cout<<"tu calificacion es aprobatoria"<<endl;
+		
+	}
+	else{
+		cout<<"lo lamento tu calificacion no es aprobatoria";
+}
+cout<<"gracias por su tiempo en la aplicacion";
+return 0;
+}
+	
+	
+	
+	
+
---
+++ tablas multi.cpp
@@ -0,0 +1,14 @@
+#include"iostream"
+using namespace std;
+int main(){
+int i;
+int num;
+cout<<"Inserta el numero de la tabla de multiplicar que deseas"<<endl;
+cin>>num;
+for(i=1;i<=50;i++){
+	cout<<(i*num)<<endl;
+}
+return 0;
+	
+	
+}
